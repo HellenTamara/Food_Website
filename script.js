42c302158abebe007233887ad8a4c04b7044d192
HellenTamara <hellen.tamara@outlook.com>
2023-01-23 12:19:11 +0900

    Final fixing

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.querySelector('.menu').addEventListener('click', ()=> {
+document.querySelector('.menu').addEventListener('click', () => {
     document.querySelectorAll('.target').forEach((item) =>{
         item.classList.toggle('change')
     })
